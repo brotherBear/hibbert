66cb607cc10c58ecc5100e4853c79b503000a7a5
Bjorn Asle Taranger <bjtar@statoil.com>
2014-04-07 14:37:55 +0200

    Removed irrelevant classes from source

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,7 +1,7 @@
 package test;
 
-import test.domain.Employee;
-import test.domain.Project;
+import org.hsqldb.util.DatabaseManagerSwing;
+
 
 
 public class Main {
@@ -9,13 +9,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Get the spring configuration
-		
-		// Create and manipulate a couple of entities.
-		Employee emp1 = Employee.create("Amadeus");
-		System.out.println(emp1);
-		
-		Project p = Project.create(emp1, "Jabba the Hutt");
-		System.out.println(p);
+		DatabaseManagerSwing mgr = new DatabaseManagerSwing();
+		mgr.main();
+		System.out.println("Finished creating database manager");
 	}
 
 }
